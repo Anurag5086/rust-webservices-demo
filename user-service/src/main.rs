c91a8672350e45dc6bec061fac604c5c3285634e
Anurag5086 <anuraggupta5086@gmail.com>
2021-12-15 12:13:20 +0400

    Routes fixed

--- user-service/src/main.rs
+++ user-service/src/main.rs
@@ -2,6 +2,7 @@ use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder,Error, 
 use serde::{Deserialize, Serialize};
 use sqlx::{query, query_as, PgPool, Pool};
 use dotenv::dotenv;
+use serde_json::json;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct User {
@@ -9,56 +10,78 @@ struct User {
     fullname: String
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
 #[post("/adduser")]
 async fn add_user(req: web::Json<User>, db_pool: web::Data<PgPool>) -> impl Responder {
+    println!("POST: /adduser");
 
     let user_new = req.into_inner();
     let tx = db_pool.get_ref();
 
-    let row = query_as!(User,
+    let _row = query!(
         r#"
         INSERT INTO users (username, fullname) VALUES
-        ($1, $2) returning username, fullname
+        ($1, $2)
         "#,
         user_new.username,
         user_new.fullname,
     )
-    .fetch_one(tx)
+    .execute(tx)
     .await;
 
-    let user: User = User {
-        username: user_new.username,
-        fullname: user_new.fullname
+    let res = match _row {
+        Err(_) => {
+            json!({
+                "status": "error",
+                "message": "User already exists!"
+            })
+        },
+        Ok(1) => {
+            json!({
+                "status": "ok",
+                "message": "User created Successfully!"
+            })
+        },
+        Ok(_) => {
+            json!({
+                "status": "error",
+                "message": "Could not create the user! Please try again!"
+            })
+        }
     };
 
-    HttpResponse::Ok().json(user)
+    HttpResponse::Ok().json(res)
 }
 
 #[get("/getuser/{name}")]
 async fn get_user(name: web::Path<String>, db_pool: web::Data<PgPool>) -> impl Responder {
+    println!("GET: /getuser/[name]");
 
-    let tx = db_pool.get_ref();
+    let new_pool = db_pool.get_ref();
     let user_name = name.to_string();
 
-    println!("{}", user_name);
-
-    let row = query_as!(User,
+    let _row = query_as!(User,
         r#"
         SELECT username, fullname from users WHERE username = $1
         "#,
         user_name
     )
-    .fetch_one(tx)
+    .fetch_one(new_pool)
     .await;
 
-    println!("{:?}", row);
+    let res = match _row {
+        Err(_) => {
+            json!({
+                "error": "User not found!"
+            })
+        },
+        Ok(user) => {
+            json!(
+                user
+            )
+        }
+    };
 
-    HttpResponse::Ok()
+    HttpResponse::Ok().json(res)
 }
 
 #[actix_web::main]
